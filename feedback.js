ecc8c3c7ebd27bb74f6b5bb549b1a5f0a1e50688
Kenneth Lewenhagen <lewenhagen@live.com>
2018-09-11 10:28:53 +0200

    lengthen the box on init

--- feedback.js
+++ feedback.js
@@ -2,9 +2,11 @@ javascript:(function(){
     let myStorage = window.localStorage;
     let users = ["Kenneth", "Andreas", "Emil", "Mikael"];
     let box = document.getElementsByClassName("ic-Input grading_comment")[0];
+    box.rows = 30;
     let textBefore = "Hej,\nVälkommen till kursen Webbteknologier aka htmlphp.\n\n";
     let feedback = "Feedback: ";
     let textAfter = "\nHär beskrivs vilken feedback du kan förvänta dig på rättningarna: https://dbwebb.se/kurser/faq/vilken-feedback-far-man-pa-inlamningarna\n\nHär finns bra beskrivningar av hur du skriver en reflekterande redovisningstext: https://dbwebb.se/kurser/faq/att-skriva-en-bra-redovisningstext\n\nKom ihåg att du alltid kan ställa frågor och få feedback i forumet (https://dbwebb.se/forum/viewforum.php?f=4) och på gitter (https://gitter.im/dbwebb-se/htmlphp).\n\n// ";
+
     let workingDiv = document.createElement("div");
     workingDiv.id = "feedbackArea";
     workingDiv.style.position = "absolute";
@@ -13,7 +15,6 @@ javascript:(function(){
     workingDiv.style.zIndex = 10000;
     workingDiv.style.width = "800px";
     workingDiv.style.height = "250px";
-
     workingDiv.innerHTML += "<div style='background-color:#2C3539;color:#fff;border-bottom:1px solid orange;width:818px;font-size:18px;margin-bottom:0;text-align:center;display:block;'>Du rättar som: <span id='user'>" + myStorage.getItem("name") + "</span></div><textarea autofocus id='feedbackText' style='width:800px;height:200px;background-color:#2C3539;font-size:18px;color:#fff;margin-bottom:0;'></textarea>\
     <div style='background-color:#2C3539;width:808px;border-top:1px solid orange;padding:5px;'>\
     <button id='pressMePlease' style='height:40px;background:#eee;'>INSERT FEEDBACK</button>\
--- feedback2.js
+++ feedback2.js
@@ -2,6 +2,7 @@ javascript:(function(){
     let myStorage = window.localStorage;
     let users = ["Kenneth", "Andreas", "Emil", "Mikael"];
     let box = document.getElementsByClassName("ic-Input grading_comment")[0];
+    box.rows = 30;
     let textBefore = "Hej,\n\n";
     let feedback = "Feedback: ";
     let textAfter = "\nHär beskrivs vilken feedback du kan förvänta dig på rättningarna: https://dbwebb.se/kurser/faq/vilken-feedback-far-man-pa-inlamningarna\n\nHär finns bra beskrivningar av hur du skriver en reflekterande redovisningstext: https://dbwebb.se/kurser/faq/att-skriva-en-bra-redovisningstext\n\nKom ihåg att du alltid kan ställa frågor och få feedback i forumet (https://dbwebb.se/forum/viewforum.php?f=4) och på gitter (https://gitter.im/dbwebb-se/htmlphp).\n\n// ";
