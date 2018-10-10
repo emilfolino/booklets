5ebec698a82ac6aa7cdfcf5561faf54bfcba73c9
Kenneth Lewenhagen <lewenhagen@live.com>
2018-10-10 11:39:19 +0200

    added even more 04 for htmlphp

--- franklin.js
+++ franklin.js
@@ -31,6 +31,7 @@ javascript:(function(){
         <option value='Jag har tittat på din inlämning och alla delarna ser bra ut. '>htmlphp 01 (intro)</option>\
         <option value='Jag har gått igenom labben och me-sidan och '>htmlphp 02 (intro)</option>\
         <option value='Jag har tittat på din inlämning och alla delarna finns på plats och fungerar som det ska. Din redovisiningstext är välskriven och reflekterande. Snyggt jobbat! '>htmlphp 04 (intro)</option>\
+        <option value='Din redovisingstext är välskriven och reflekterande. '>Bra redovisningtext</option>\
         <option value='Bra jobbat! '>Bra jobbat!</option>\
         <option value='Härligt att du gjort extrauppgifterna! '>Extrauppgifter ok</option>\
         <option value='Vänligen komplettera följande: '>Komplettering</option>\
