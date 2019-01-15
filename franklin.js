f420a0889740e885ae37f76cb45206bb1db4ef4b
Kenneth Lewenhagen <lewenhagen@live.com>
2019-01-15 11:29:57 +0100

    added matris link stuff

--- franklin.js
+++ franklin.js
@@ -31,7 +31,7 @@ javascript:(function(){
         <option value='Jag har gått igenom din inlämning och tittat på labben, sandboxen och din me-sida. Alla delarna fungerar fint och enligt kraven. Kör på med nästa moment!'>js1 01 (intro)</option>\
         <option value='Jag har testat de olika tangenbordsinmatningarna och allt ser bara bra ut. Din text är reflekterande och väl skriven. Kör så det ryker med nästa kursmoment!'>js1 05</option>\
         <option value='Jag har spelat ditt Hangman och det fungerar fint. Alla delarna är på plats och rätt metoder är publika. Mycket bra jobbat! Kämpa på nu och lycka till med projektet.'>js1 06</option>\
-        <option value='Jag har tittat på ditt projekt. Bra jobbat med både koden och presentationen! Du kan se matrisen för betyget om du klickar på länken 'visa matris'. Svara gärna på kursvärderingen som bör komma strax efter kursens slut då den ligger till stöd för kommande versioner av kursen.'>js1 10</option>\
+        <option value='Jag har tittat på ditt projekt. Bra jobbat med både koden och presentationen! Du kan se matrisen för betyget om du klickar på länken ovan. Svara gärna på kursvärderingen som bör komma strax efter kursens slut då den ligger till stöd för kommande versioner av kursen.'>js1 10</option>\
         <option value='Jag har tittat på din inlämning och alla delarna ser bra ut. '>htmlphp 01 (intro)</option>\
         <option value='Jag har gått igenom labben och me-sidan och '>htmlphp 02 (intro)</option>\
         <option value='Jag har tittat på din inlämning och alla delarna finns på plats och fungerar som det ska. Din redovisningstext är välskriven och reflekterande. Snyggt jobbat! '>htmlphp 04 (intro)</option>\
