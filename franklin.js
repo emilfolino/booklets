a388de5bd5e11b7fe22302d9ce41d68dd81f8e7a
Kenneth Lewenhagen <lewenhagen@live.com>
2020-01-29 10:19:48 +0100

    added oopython kmom01 opt 3

--- franklin.js
+++ franklin.js
@@ -52,6 +52,7 @@ javascript:(function(){
         <option value='Din inlämning visar på en bra förståelse på skillnaden mellan de olika datastrukturerna. Den är även enligt specikationen och fungerar som det ska. Bra jobbat och lyckat till med examinationen.'>Kmom06 (python)</option>\
         <option value='Dina klasser ser bra och korrekta ut. Efter att ha läst din redovisningstext och kollat på din kod känns det som att veta vad klasser och objekt är. Bra jobbat!.'>kmom01 (oopython)</option>\
         <option value='Jag har inspekterat dina klasser och labben. Klasserna ser ut som det är tänkt. Du har en bra förklaring på klasser och objekt, det ser ut som att du förstår klasser och objekt.'>kmom01 (oopython) alt. 2</option>\
+        <option value='Jag har tittat på labben och Flask-applikationen och läst igenom din redovisningstext. Det verkar som om du har koll på klasser och objekt! Din Flaskapplikation snurrar också på fint med korrekt innehåll enligt instruktionerna. Bra jobbat - kör på med nästa kursmoment!'>kmom01 (oopython) alt. 3</option>\
         </select>\
         <select id='names'>\
         <option value=''>Vem är du?</option>\
