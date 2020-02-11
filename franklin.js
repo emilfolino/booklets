798e6eed8ad18e54db8476770b19feeeffc3d8ee
Andreas Arnesson <andreas_osby@hotmail.com>
2020-02-11 10:54:51 +0100

    oopython kmom03

--- franklin.js
+++ franklin.js
@@ -54,6 +54,7 @@ javascript:(function(){
         <option value='Jag har inspekterat dina klasser och labben. Klasserna ser ut som det är tänkt. Du har en bra förklaring på klasser och objekt, det ser ut som att du förstår klasser och objekt.'>kmom01 (oopython) alt. 2</option>\
         <option value='Jag har tittat på labben och Flask-applikationen och läst igenom din redovisningstext. Det verkar som om du har koll på klasser och objekt! Din Flaskapplikation snurrar också på fint med korrekt innehåll enligt instruktionerna. Bra jobbat - kör på med nästa kursmoment!'>kmom01 (oopython) alt. 3</option>\
         <option value='Labben ser bra ut och frågersporten fungerar som den ska. Koden och klasserna är bra gjorda, du har uttnytja arv på rätt sätt och inte skapat onödiga metoder i Question klasserna. Bra förklaringar av de olika koncepten i redovisningtexten. '>kmom02 (oopython)</option>\
+        <option value='Du har bra struktur i din kod och använder dina klasser på ett bra sätt. War klassen kunde haft mer metoder och inte bara en stor. Din tester kunde varit lite mer utförliga och även testat vad som händer när något går fel. Klassdiagrammet ser bra ut.'>kmom03 (oopython)</option>\
         </select>\
         <select id='names'>\
         <option value=''>Vem är du?</option>\
